dd69d915c17f27ca338edacb5a20477fd9ab17f5
Chase Hardin <chasehardin@Chases-MacBook-Pro.local>
2017-08-03 15:56:30 -0500

    Initial commit with Hello World

---
+++ web/main.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+import 'src/features.dart';
+import 'package:over_react/over_react.dart';
+import 'package:react/react_client.dart' show setClientConfiguration;
+import 'package:react/react_dom.dart' as react_dom;
+
+void main() {
+  setClientConfiguration();
+
+  react_dom.render((HelloWorld()..message="Hello World!")(), querySelector('.hello-world'));
+}
---
+++ web/src/features.dart
@@ -0,0 +1,5 @@
+library over_react.web.features;
+
+import 'package:over_react/over_react.dart';
+
+part 'features/hello-world/hello-world.dart';
