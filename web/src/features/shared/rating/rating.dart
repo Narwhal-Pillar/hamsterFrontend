bd856d3f66ca5a4e70b3b9b69d721d67997b360e
Noe Azar <noeazar@Noes-MacBook-Pro.local>
2017-08-10 14:22:24 -0500

    Passing starts test for rating component

--- web/src/features/shared/rating/rating.dart
+++ web/src/features/shared/rating/rating.dart
@@ -5,7 +5,7 @@ UiFactory<RatingProps> Rating;
 
 @Props()
 class RatingProps extends UiProps {
-  
+  double rating;
 }
 
 @State()
@@ -16,12 +16,21 @@ class RatingState extends UiState {
 @Component()
 class RatingComponent<T extends RatingProps, S extends RatingState> extends UiStatefulComponent<T, S> {
   render() {
+    List stars = [];
+    double x = this.props.rating;
+    for(var i = 0; i < 5; i++) {
+      if(x >= 1) {
+        stars.add((Dom.i()..className='fa fa-star full-star')());
+        x--;
+      } else if(x >=.5) {
+        stars.add((Dom.i()..className='fa fa-star-half-o empty-star')());
+        x -= 0.5;
+      } else {
+        stars.add((Dom.i()..className='fa fa-star-o empty-star')());
+      }
+    }
     return (Dom.span()(
-      (Dom.i()..className='fa fa-star-o')(),
-      (Dom.i()..className='fa fa-star-o')(),
-      (Dom.i()..className='fa fa-star-o')(),
-      (Dom.i()..className='fa fa-star-o')(),
-      (Dom.i()..className='fa fa-star-o')()
+      stars
     ));
   }
 }
--- web/src/features/shared/rating/rating_test.dart
+++ web/src/features/shared/rating/rating_test.dart
@@ -5,23 +5,38 @@ import '../shared.dart';
 void main(){
   group('Rating tests', () {
     test('Should render rating component', () {
-      var instance = render((Rating())());
-
+      var instance = render((Rating()..rating=0.0)());
       RatingComponent component = getDartComponent(instance);
-
       var renderedComponent = findDomNode(component);
 
       expect(renderedComponent.toString(), 'span');
     });
 
-     test('Should render five stars', () {
-      var instance = render((Rating())());
-
+    test('Should render five stars', () {
+      var instance = render((Rating()..rating=0.0)());
       RatingComponent component = getDartComponent(instance);
-
       var renderedComponent = findDomNode(component);
 
       expect(renderedComponent.children.length, 5);
+    });
+
+    test('Should render five stars', () {
+      var instance = render((Rating()..rating=0.0)());
+      RatingComponent component = getDartComponent(instance);
+      var renderedComponent = findDomNode(component);
+
+      expect(renderedComponent.children.length, 5);
+    });
+
+
+
+    test('Should render 0 star rating', () {
+      var instance = render((Rating()..rating=0.0));
+      RatingComponent component = getDartComponent(instance);
+      var renderedComponent = findDomNode(component);
+
+      expect(renderedComponent.querySelectorAll('.empty-star').length, 5);
+      expect(renderedComponent.querySelectorAll('.full-star').length, 0);
     });
   });
 }
