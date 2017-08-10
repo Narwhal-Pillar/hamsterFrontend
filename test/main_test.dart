80724d201533879caefd42b58bfa942d5f300424
Chase Hardin <chasehardin@Chases-MacBook-Pro.local>
2017-08-10 09:41:08 -0500

    Add restaurant components.

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,12 +1,12 @@
 @TestOn('browser')
 import 'package:over_react/over_react.dart';
-import 'package:react/react_dom.dart';
 import 'package:react/react_client.dart';
 import 'package:test/test.dart';
 
 import '../web/src/features/hello_world/hello_world_test.dart' as hello_world;
 import '../web/src/features/top_nav/top_nav_test.dart' as top_nav;
 import '../web/src/features/app_container/app_container_test.dart' as app_container;
+import '../web/src/features/restaurant/restaurant_test.dart' as restaurant;
 
 
 void main() {
@@ -15,4 +15,5 @@ void main() {
   hello_world.main();
   top_nav.main();
   app_container.main();
+  restaurant.main();
 }
