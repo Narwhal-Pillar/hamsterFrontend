f16fae911b3773a2bd19fee8264872dc68b76495
Noe Azar <noeazar@Noes-MacBook-Pro.local>
2017-08-11 10:47:55 -0500

    Remove HelloWorld Component.

--- test/main_test.dart
+++ test/main_test.dart
@@ -3,7 +3,6 @@ import 'package:over_react/over_react.dart';
 import 'package:react/react_client.dart';
 import 'package:test/test.dart';
 
-import '../web/src/features/hello_world/hello_world_test.dart' as hello_world;
 import '../web/src/features/top_nav/top_nav_test.dart' as top_nav;
 import '../web/src/features/app_container/app_container_test.dart' as app_container;
 import '../web/src/features/shared/food_type/food_type_text_test.dart' as food_type_text;
@@ -15,7 +14,6 @@ import '../web/src/features/place_details/place_details_test.dart' as place_deta
 void main() {
   setClientConfiguration();
 
-  hello_world.main();
   top_nav.main();
   app_container.main();
   food_type_text.main();
--- web/src/features.dart
+++ web/src/features.dart
@@ -3,7 +3,6 @@ library over_react.web.features;
 import 'package:over_react/over_react.dart';
 import 'features/shared/shared.dart';
 
-part 'features/hello_world/hello_world.dart';
 part 'features/top_nav/top_nav.dart';
 part 'features/app_container/app_container.dart';
 part 'features/place_details/place_details.dart';
